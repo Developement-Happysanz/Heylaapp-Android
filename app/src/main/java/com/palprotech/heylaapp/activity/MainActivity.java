e621610bc3801a03e6a43fc42aa1abcbb787ea7e
Nandhakumar <nandhakumaar.n@gmail.com>
2017-09-23 10:34:25 +0530

    Initial commit

---
+++ app/src/main/java/com/palprotech/heylaapp/activity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.palprotech.heylaapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.palprotech.heylaapp.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
