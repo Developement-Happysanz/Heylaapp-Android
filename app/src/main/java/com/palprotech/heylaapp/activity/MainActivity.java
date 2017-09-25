02bb50b66668f4d58d334dd5891d7ebaf38d7450
Nandhakumar <nandhakumaar.n@gmail.com>
2017-09-25 17:45:34 +0530

    FB login update

--- app/src/main/java/com/palprotech/heylaapp/activity/MainActivity.java
+++ app/src/main/java/com/palprotech/heylaapp/activity/MainActivity.java
@@ -1,10 +1,18 @@
 package com.palprotech.heylaapp.activity;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 
 import com.palprotech.heylaapp.R;
 import com.palprotech.heylaapp.fcm.SharedPrefManager;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +25,21 @@ public class MainActivity extends AppCompatActivity {
 //        final String token = SharedPrefManager.getInstance(this).getDeviceToken();
 //
 //        String ok = token;
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.palprotech.heylaapp",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
 
     }
 }
