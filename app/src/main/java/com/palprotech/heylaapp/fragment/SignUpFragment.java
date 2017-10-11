39ec8545a44f09eb02189ac4c939227e0c69a4e0
Narendar <naren.happysanz@gmail.com>
2017-10-11 13:30:31 +0530

    Login Page

--- app/src/main/java/com/palprotech/heylaapp/fragment/SignUpFragment.java
+++ app/src/main/java/com/palprotech/heylaapp/fragment/SignUpFragment.java
@@ -2,7 +2,10 @@ package com.palprotech.heylaapp.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -34,6 +37,32 @@ public class SignUpFragment extends Fragment {
 
         rootView = inflater.inflate(R.layout.fragment_sign_up, container, false);
         initializeViews();
+        password.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_login_password,0,R.drawable.ic_login_show_password,0);
+        password.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int DRAWABLE_LEFT = 0;
+                final int DRAWABLE_TOP = 1;
+                final int DRAWABLE_RIGHT = 2;
+                final int DRAWABLE_BOTTOM = 3;
+                Boolean hide = true;
+
+                if(event.getAction() == MotionEvent.ACTION_UP) {
+                    if(event.getRawX() >= (password.getRight() - password.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                        password.setTransformationMethod(new PasswordTransformationMethod());
+                        password.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_login_password,0,R.drawable.ic_login_hide_password,0);
+
+                        return true;
+                    }
+                }
+                else if(event.getAction() == MotionEvent.ACTION_DOWN){
+                    password.setTransformationMethod(new HideReturnsTransformationMethod());
+                    password.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_login_password,0,R.drawable.ic_login_show_password,0);
+                    return true;
+                }
+                return false;
+            }
+        });
 
         return rootView;
     }
