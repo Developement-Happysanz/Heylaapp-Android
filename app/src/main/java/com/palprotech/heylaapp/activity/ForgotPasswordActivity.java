96379c6cc8f489db96573aaea0d6685e8abc2904
Narendar <naren.happysanz@gmail.com>
2017-10-12 11:59:18 +0530

    Login Page

---
+++ app/src/main/java/com/palprotech/heylaapp/activity/ForgotPasswordActivity.java
@@ -0,0 +1,18 @@
+package com.palprotech.heylaapp.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.palprotech.heylaapp.R;
+
+/**
+ * Created by Narendar on 12/10/17.
+ */
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
