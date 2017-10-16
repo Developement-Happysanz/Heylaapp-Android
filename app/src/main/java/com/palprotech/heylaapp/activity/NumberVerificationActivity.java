6bb07da37cadb11bb9a43c4a04a86058883d9df8
Narendar <naren.happysanz@gmail.com>
2017-10-16 12:22:32 +0530

    Login Page

---
+++ app/src/main/java/com/palprotech/heylaapp/activity/NumberVerificationActivity.java
@@ -0,0 +1,18 @@
+package com.palprotech.heylaapp.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.palprotech.heylaapp.R;
+
+/**
+ * Created by Narendar on 16/10/17.
+ */
+
+public class NumberVerificationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_number_verification);
+    }
+}
