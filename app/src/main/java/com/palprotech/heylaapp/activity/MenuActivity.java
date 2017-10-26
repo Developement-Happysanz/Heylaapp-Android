f6660b8e23208e4968c117584faab4e9a9b14f4f
Narendar <naren.happysanz@gmail.com>
2017-10-26 22:55:37 +0530

    Menu Design Android studio 3.0 gradle changes

--- app/src/main/java/com/palprotech/heylaapp/activity/MenuActivity.java
+++ app/src/main/java/com/palprotech/heylaapp/activity/MenuActivity.java
@@ -14,6 +14,6 @@ public class MenuActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_change_number);
+        setContentView(R.layout.activity_menu);
     }
 }
