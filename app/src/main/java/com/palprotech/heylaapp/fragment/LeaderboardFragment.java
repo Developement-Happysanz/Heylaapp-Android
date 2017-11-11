ccaeb87ee68babb9ff8b32187f9df0dbcc265bb4
Narendar <naren.happysanz@gmail.com>
2017-11-11 15:16:10 +0530

    Keyboard dismiss on touch

--- app/src/main/java/com/palprotech/heylaapp/fragment/LeaderboardFragment.java
+++ app/src/main/java/com/palprotech/heylaapp/fragment/LeaderboardFragment.java
@@ -5,22 +5,57 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.palprotech.heylaapp.R;
+import com.palprotech.heylaapp.customview.CircleImageView;
 
 /**
  * Created by Narendar on 28/10/17.
  */
 
-public class LeaderboardFragment extends Fragment {
+public class LeaderboardFragment extends Fragment implements View.OnClickListener {
 
-    private View rootView;
+    View rootView;
+    CircleImageView profileImg;
+    Button follow;
+    TextView viewFullStatistics;
+    ProgressBar share, engagement, checkin, booking, friends;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         rootView = inflater.inflate(R.layout.fragment_leaderboard, container, false);
 
+        initializeViews();
+
         return rootView;
+    }
+
+    protected void initializeViews() {
+        share = rootView.findViewById(R.id.progress_sharing);
+        share.setProgress(0);
+        engagement = rootView.findViewById(R.id.progress_engagement);
+        engagement.setProgress(0);
+        checkin = rootView.findViewById(R.id.progress_checkin);
+        checkin.setProgress(0);
+        booking = rootView.findViewById(R.id.progress_booking);
+        booking.setProgress(0);
+        friends = rootView.findViewById(R.id.progress_friends);
+        friends.setProgress(0);
+        profileImg = rootView.findViewById(R.id.profile_img);
+        profileImg.setOnClickListener(this);
+        follow = rootView.findViewById(R.id.follow);
+        follow.setOnClickListener(this);
+        viewFullStatistics = rootView.findViewById(R.id.view_statistics);
+        viewFullStatistics.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
