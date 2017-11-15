6dcb93bc843119c2eefe7bfe5df35c198d2eed7f
Narendar <naren.happysanz@gmail.com>
2017-11-15 10:59:30 +0530

    Keyboard dismiss on touch

--- app/src/main/java/com/palprotech/heylaapp/activity/ProfileActivity.java
+++ app/src/main/java/com/palprotech/heylaapp/activity/ProfileActivity.java
@@ -1,5 +1,6 @@
 package com.palprotech.heylaapp.activity;
 
+import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
 import android.content.ComponentName;
@@ -156,6 +157,35 @@ public class ProfileActivity extends AppCompatActivity implements AdapterView.On
         return true;
     }
 
+    public static void hideSoftKeyboard(Activity activity) {
+        InputMethodManager inputMethodManager =
+                (InputMethodManager) activity.getSystemService(
+                        Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(
+                activity.getCurrentFocus().getWindowToken(), 0);
+    }
+
+    public void setupUI(View view) {
+
+        // Set up touch listener for non-text box views to hide keyboard.
+        if (!(view instanceof EditText)) {
+            view.setOnTouchListener(new View.OnTouchListener() {
+                public boolean onTouch(View v, MotionEvent event) {
+                    hideSoftKeyboard(ProfileActivity.this);
+                    return false;
+                }
+            });
+        }
+
+        //If a layout container, iterate over children and seed recursion.
+        if (view instanceof ViewGroup) {
+            for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
+                View innerView = ((ViewGroup) view).getChildAt(i);
+                setupUI(innerView);
+            }
+        }
+    }
+
     void setUI() {
 
         serviceHelper = new ServiceHelper(this);
@@ -175,7 +205,7 @@ public class ProfileActivity extends AppCompatActivity implements AdapterView.On
         if (((url != null) && !(url.isEmpty()))) {
             Picasso.with(this).load(url).placeholder(R.drawable.ic_default_profile).error(R.drawable.ic_default_profile).into(mProfileImage);
         }
-
+        setupUI(findViewById(R.id.scrollID));
         inputName = (TextInputLayout) findViewById(R.id.ti_name);
         name = (EditText) findViewById(R.id.edtName);
         inputUsername = (TextInputLayout) findViewById(R.id.ti_username);
